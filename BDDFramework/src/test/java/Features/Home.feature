adc14a65d28511973ea011520c55909184654260
Mangesh Arvind Shirodkar <shmangesh@altimetrik.com>
2024-03-11 10:49:16 +0530

    added login scenario

--- BDDFramework/src/test/java/Features/Home.feature
+++ BDDFramework/src/test/java/Features/Home.feature
@@ -1,26 +1,8 @@
- Feature: "Application login"
-    @tag
-    Scenario: verify login page logo
-      Given navigate to home page URL "ExtendedHomeUrl"
-      When  Click on Register user
-      Then  verify Register page Title
-      
-      
-      
-      
-  
-   
-  
-      
-      
-    
-      
-      
-      
-      
-      
-   
-      
-     
-      
-      
+Feature: "Application login"
+  @Login
+  Scenario: Verify user login is Successful with valid credentials.
+    Given navigate to home page
+    When  Click on link for Log In
+    And User enters email and password
+    And user clicks on continue
+    Then verify user logged in successfully modal is displayed.
--- BDDFramework/src/test/java/PageObjects/HomePage.java
+++ BDDFramework/src/test/java/PageObjects/HomePage.java
@@ -6,23 +6,53 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import CommonUtility.BaseClass;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage{
-	
+
 	WebDriver driver ;
-	
+
+	@FindBy(xpath = "//a[normalize-space()='Log in']")
+	WebElement btnLogin;
+
+	@FindBy(id = "email")
+	WebElement txtEmail;
+
+	@FindBy(id = "password")
+	WebElement txtPassword;
+
+	@FindBy(xpath = "//button[@type='submit']")
+	WebElement btnContinue;
+
+	@FindBy(xpath = "//div[@role='dialog']")
+	WebElement lblLogin;
+
 	public HomePage(WebDriver driver) {
-		
+
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
-		
+
 	}
-	
-//	@FindBy(xpath = "(//a[@amplitude-id='menu-signup'])[2]")
-//	WebElement linkRegister;
-	
-//	public void clickOnRegisterLink() {
-//		linkRegister.click();
-//	}
+	public void clickLogin(){
+		btnLogin.click();
+	};
+
+	public void enterEmailAndPassword(){
+		txtEmail.sendKeys("Testemail@gmail.com");
+		txtPassword.sendKeys("Test@123");
+	}
+
+	public void clickContinue(){
+		btnContinue.click();
+	}
+
+	public void isPopupDisplayed(){
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(lblLogin));
+		lblLogin.isDisplayed();
+	}
 
 }
